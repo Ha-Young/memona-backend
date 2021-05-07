25af145bcf07d7cfbd53559ddd6fb9b751737041
ha-young <hayeong28@naver.com>
2021-05-08 01:08:37 +0900

    [ADD] user schema

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  posts: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    }],
+    default: [],
+  },
+  friends: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
