46967664ed081881c452cca7d5c2b776267698db
ha-young <hayeong28@naver.com>
2021-05-08 01:49:36 +0900

    [ADD] User GraphQl Schema

---
+++ graphql/schema/User/index.js
@@ -0,0 +1,15 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    email: String!
+    username: String!
+    nickname: String
+    avatar: String
+    posts: [Post]
+    friends: [User]
+  }
+`;
+
+module.exports = typeDefs;
