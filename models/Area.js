2bb7270692bda3967012e33de56f4106ab6d2b53
ha-young <hayeong28@naver.com>
2021-05-08 01:24:22 +0900

    [ADD] Area schema

---
+++ models/Area.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const { polygonSchema } = require("./GeoJsonSubSchemas");
+
+const areaSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+  },
+  location: {
+    type: polygonSchema,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Area", areaSchema);
