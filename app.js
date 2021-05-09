a9b0613517a2eb1ec84d43a4e6c89c828dc8fd11
ha-young <hayeong28@naver.com>
2021-05-09 23:56:19 +0900

    [ADD] apolloServer context - authentication logic

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
 const dataSources = require("./graphql/datasource");
-
-console.log("typeDefs", typeDefs);
-console.log("resolvers", resolvers);
+const { verifyJWTToken } = require("./utils/jwtToken");
+const extractToken = require("./utils/extractToken");
+const UserModel = require("./models/User");
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources,
+  context: async ({ req }) => {
+    const token = extractToken(req);
+
+    if (!token) {
+      return { user: undefined };
+    }
+
+    const verifiedResult = verifyJWTToken(token);
+
+    const user = await UserModel.find({ email: verifiedResult.email });
+
+    return { user };
+  },
 });
 
 module.exports = apolloServer;
