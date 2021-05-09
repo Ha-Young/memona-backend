365d5a8ebaa83c19af84d49e0b9bb4a1b33852ea
ha-young <hayeong28@naver.com>
2021-05-09 21:57:01 +0900

    [FIX] wrong syntax in Users MongoDataSource

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require("apollo-server");
 
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
+const Users = require("./graphql/datasource/Users");
+const UserModel = require("./models/User");
 
 console.log("typeDefs", typeDefs);
 console.log("resolvers", resolvers);
@@ -9,6 +11,9 @@ console.log("resolvers", resolvers);
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  dataSources: () => ({
+    users: new Users(UserModel),
+  }),
 });
 
 module.exports = apolloServer;
