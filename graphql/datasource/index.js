189b25867995633e1fa1e92407572e4df20a2a2d
ha-young <hayeong28@naver.com>
2021-05-11 00:15:46 +0900

    [ADD] graphql datasource - area

--- graphql/datasource/index.js
+++ graphql/datasource/index.js
@@ -1,9 +1,12 @@
 const Users = require("./Users");
+const Areas = require("./Areas");
 const UserModel = require("../../models/User");
+const AreaModel = require("../../models/Area");
 
 function dataSources() {
   return {
     users: new Users(UserModel),
+    areas: new Areas(AreaModel),
   };
 }
 
