f667621af14f3b5f4fd45e8fdc488e8c56c173cd
ha-young <hayeong28@naver.com>
2021-05-11 02:36:02 +0900

    [ADD] graphql datasource - kakaoAPI for unregistered area

--- graphql/datasource/index.js
+++ graphql/datasource/index.js
@@ -2,11 +2,13 @@ const Users = require("./Users");
 const Areas = require("./Areas");
 const UserModel = require("../../models/User");
 const AreaModel = require("../../models/Area");
+const KakaoAPI = require("./KakaoAPI");
 
 function dataSources() {
   return {
     users: new Users(UserModel),
     areas: new Areas(AreaModel),
+    kakaoAPI: new KakaoAPI(),
   };
 }
 
