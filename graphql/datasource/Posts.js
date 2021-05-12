1a8996d98f64594b2aa0798c121c8628d07bb8f7
ha-young <hayeong28@naver.com>
2021-05-13 03:35:27 +0900

    [ADD] apply mongoose-aggregate-paginate to posts datasource and post model

--- graphql/datasource/Posts.js
+++ graphql/datasource/Posts.js
@@ -13,9 +13,35 @@ class Posts extends MongoDataSource {
   getPostsByIds(ids) {
     return this.findManyByIds(ids);
   }
+  getRandomPostsWithPagenation({ query, pagingOption }) {
+    // https://docs.mongodb.com/manual/aggregation/#std-label-aggregation-framework
+    const aggregateQuery = this.model.aggregate([{ $match: query }]);
+
+    return this.model.aggregatePaginate(aggregateQuery, pagingOption);
+  }
   createPost(post) {
     return this.model.create(post);
   }
 }
 
 module.exports = Posts;
+
+// {
+//   "_id": {
+//     "$oid": "609c1cf6b1186a7b3c97604c"
+//   },
+//   "author": {
+//     "$oid": "60978fb4597e45176f27c9c1"
+//   },
+//   "content": "test테스트입니다테스트",
+//   "postImageUrl": "dsfsdfsdfdsf/dsfsdfsdf.png",
+//   "location": {
+//     "type": "Point",
+//     "coordinate": [123, 37]
+//   },
+//   "isAnonymous": true,
+//   "area": "역삼동",
+//   "season": "winter",
+//   "comments": [],
+//   "likes": []
+// }
