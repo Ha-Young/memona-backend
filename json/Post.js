67b7ce9ac3156dbfbf0cfc398527ffdd13c0b179
ha-young <hayeong28@naver.com>
2021-05-16 02:33:30 +0900

    [ADD] post mock data creation

---
+++ json/Post.js
@@ -0,0 +1,37 @@
+exports.imageNumbering = 74;
+
+exports.locationMocks = [
+  {
+    name: "서초동",
+    location: {
+      type: "Point",
+      coordinates: [127.02928809999999, 37.4923615],
+    },
+  },
+  {
+    name: "역삼역",
+    location: {
+      type: "Point",
+      coordinates: [127.035707, 37.499765],
+    },
+  }
+];
+
+exports.seasons = [
+  "spring",
+  "summer",
+  "autumn",
+  "winter"
+];
+
+exports.years = [
+  2021,
+  2020,
+  2019,
+  2018,
+  2017,
+  2016,
+  2015,
+  2014,
+  2013
+];
--- json/saveDB.js
+++ json/saveDB.js
@@ -1,13 +1,47 @@
+const mongoose = require("mongoose");
 const Area = require("../models/Area");
+const Post = require("../models/Post");
 
 const jsonAreas = require("./Area.json");
 
-module.exports = function saveDB() {
+const { imageNumbering, locationMocks, seasons, years } = require("./Post");
+
+exports.saveAreas = function saveAreas() {
   console.log("area json data save db...");
   const promises = [];
 
   for (const area of jsonAreas.Area) {
     promises.push(Area.updateOne({ name: area.name }, area, { upsert: true }));
+  }
+
+  return Promise.all(promises);
+};
+
+exports.savePosts = function savePosts() {
+  console.log("post data save db...");
+  const promises = [];
+
+  for (let number = 1; number <= imageNumbering; number++) {
+    const postImageMock = `https://memona.s3.ap-northeast-2.amazonaws.com/test${number}.jpeg`;
+    const locationIndex = Math.floor(Math.random() * locationMocks.length);
+    const locationMock = locationMocks[locationIndex];
+    const season = seasons[Math.floor(Math.random() * seasons.length)];
+    const year = years[Math.floor(Math.random() * years.length)];
+
+    console.log(locationIndex, locationMock);
+
+    const postMock = {
+      author: mongoose.Types.ObjectId("60978fb4597e45176f27c9c1"),
+      content: `테스트 목 데이터 입니다.\n${locationMock.name}\n${season}\n${year}`,
+      postImageUrl: postImageMock,
+      location: locationMock.location,
+      area: locationMock.name,
+      isAnonymous: true,
+      season,
+      year,
+    };
+
+    promises.push(Post.create(postMock));
   }
 
   return Promise.all(promises);
