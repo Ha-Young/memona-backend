63501f4ac4e2455c014069cbbdb7674d914b7ed2
hychoi <hayeong28@naver.com>
2021-06-09 18:20:42 +0900

    [ADD] graphQL mutation description

--- graphql/schema/Mutation/index.js
+++ graphql/schema/Mutation/index.js
@@ -2,7 +2,10 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Mutation {
+    "Mutation to login. if type is one of social login, must need token argument."
     login(type: String, token: String, email: String, password: String): String
+
+    "Mutation to create post. It is recommended that the file size does not exceed 2 MB."
     createPost(input: PostInput!, file: Upload): Post!
   }
 `;
